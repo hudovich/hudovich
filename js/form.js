c8cb998a8cfddbae4cbf5b2ddc6dab4407113650
Vitalii <93428086+hudovich@users.noreply.github.com>
2024-11-06 11:57:30 +0100

    Update form.js

--- js/form.js
+++ js/form.js
@@ -86,7 +86,7 @@ async function sendFormData(formData) {
 async function sendMessageToTelegram(message) {
     const token = '8011741715:AAHovNfMxwbBOjG1lAyuSbSjGYSu5o3DVxU'; // Замените на ваш токен
     const chatId = '8011741715'; // Замените на ваш chat ID
-    const url = `https://api.telegram.org/bot${token}/sendMessage`;
+    const url = `https://api.telegram.org/bot8011741715:AAHovNfMxwbBOjG1lAyuSbSjGYSu5o3DVxU/sendMessage`;
 
     const payload = {
         chat_id: chatId,
