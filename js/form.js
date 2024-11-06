032fdfff4206ed1ff5f923db3531f8ca0b0ec605
Vitalii <93428086+hudovich@users.noreply.github.com>
2024-11-06 11:34:34 +0100

    Update form.js

--- js/form.js
+++ js/form.js
@@ -83,7 +83,7 @@ async function sendFormData(formData) {
     }
 }
 
-async function sendMessageToTelegram(message) {'
+async function sendMessageToTelegram(message) {
     const proxyUrl = "https://cors-anywhere.herokuapp.com/";
     const token = 'AAHovNfMxwbBOjG1lAyuSbSjGYSu5o3DVxU'; // Замените на ваш токен
     const chatId = '8011741715'; // Замените на ваш chat ID
