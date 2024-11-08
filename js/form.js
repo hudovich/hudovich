3c53196e4b4259387098d229df821fce34ba70c1
Vitalii <93428086+hudovich@users.noreply.github.com>
2024-11-08 12:11:08 +0100

    Update form.js

--- js/form.js
+++ js/form.js
@@ -41,7 +41,7 @@ btnOrders.forEach((btn, index) => {
             e.preventDefault();
             const formData = new FormData(form);
             // const message = document.getElementById('message').value; // Получаем сообщение
-            console.log(formData);
+            console.log(JSON.stringify(formData));
             sendMessageToTelegram(formData); // Отправляем сообщение telegram
             sendFormData(formData)
             form.reset();
